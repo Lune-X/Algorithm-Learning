8f87f92abccb17a06a04174b33f9f2ff454ea582
Lune-X <xuyichen620@gmail.com>
2024-08-19 11:59:21 +0800

    DAY ONE LEARNING

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# Test
-Test for the IDEA
+# 					Algorithm Learning outcomes
+
+## Day1: Bubble Sort
+
+双重循环控制数组遍历
+
+### 外层循环：
+
+外层循环的条件是：
+
+```
+for (int i = 0; i < array.length - 1; i++) {
+```
+
+对于长度为 8 的数组，`array.length - 1` 就是 `7`，所以 `i` 的取值范围是 `0` 到 `6`。外层循环控制着执行排序的轮数，最多需要 `array.length - 1` 次轮次排序。
+
+### 内层循环：
+
+内层循环的条件是：
+
+```
+for (int j = 0; j < array.length - 1 - i; j++) {
+```
+
+`j` 从 `0` 开始，一直到 `array.length - 1 - i`，这个条件控制了相邻元素的比较范围。每一轮都会把一个最大的元素“冒泡”到数组的末尾，因此，随着 `i` 的增加，内层循环的比较范围逐渐缩小，因为数组末尾已经排好序的部分不再需要比较。
---
+++ src/main/java/com/xyc/BubbleSort.java
@@ -0,0 +1,41 @@
+package com.xyc;
+
+import java.util.Scanner;
+
+public class BubbleSort {
+
+    public static void bubbleSort(int[] array){
+        System.out.println("array的长度：" + array.length);
+        for(int i = 0; i < array.length - 1; i++){
+            for(int j = 0; j < array.length - 1 - i; j++){
+                if(array[j] > array[j+1]){
+                    int temp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        //输入原数组
+        Scanner sc = new Scanner(System.in);
+        System.out.println("请输入数组的大小: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        System.out.println("请输入随机顺序的数组: ");
+        for(int i = 0; i< n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        //冒泡排序
+        bubbleSort(arr);
+
+        //排序后的数组
+        System.out.println("排序后的数组：");
+        for (int e:arr) {
+            System.out.println(e);
+        }
+    }
+}
