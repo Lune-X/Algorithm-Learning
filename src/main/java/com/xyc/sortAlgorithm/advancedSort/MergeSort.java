0fe6b5e1dc5dc0dff5bbbbade08120700151c552
Lune-X <xuyichen620@gmail.com>
2024-08-27 17:05:46 +0800

    DAY FOUR Algorithm Learning update Merge Sort

--- src/main/java/com/xyc/sortAlgorithm/advancedSort/MergeSort.java
+++ src/main/java/com/xyc/sortAlgorithm/advancedSort/MergeSort.java
@@ -4,68 +4,61 @@ import java.util.Scanner;
 
 public class MergeSort {
 
-    // 归并排序的主函数
-    public static void mergeSort(int[] arr, int left, int right) {
-        if (left < right) {
-            // 找到中间点
-            int mid = left + (right - left) / 2;
-
-            // 递归对左半部分排序
-            mergeSort(arr, left, mid);
-            // 递归对右半部分排序
-            mergeSort(arr, mid + 1, right);
-
-            // 合并两个有序的子数组
-            merge(arr, left, mid, right);
-        }
-    }
-
-    // 合并两个有序子数组的函数
-    public static void merge(int[] arr, int left, int mid, int right) {
-        // 找到两个子数组的大小
-        int n1 = mid - left + 1;
-        int n2 = right - mid;
-
-        // 创建临时数组，分别存储左右子数组
-        int[] leftArray = new int[n1];
-        int[] rightArray = new int[n2];
-
-        // 将数据复制到临时数组
-        for (int i = 0; i < n1; i++) {
-            leftArray[i] = arr[left + i];
-        }
-        for (int j = 0; j < n2; j++) {
-            rightArray[j] = arr[mid + 1 + j];
+    // 递归将原数组分成大小为1的小数组，然后进行排序
+    public static void mergeSort(int[] arr) {
+        int mid = arr.length / 2;
+        if (arr.length < 2) {
+            return;
         }
 
-        // 归并两个有序的子数组
-        int i = 0, j = 0;
-        int k = left;
+        int[] leftArray = new int[mid];
+        int[] rightArray = new int[arr.length-mid];
 
-        // 将两个临时数组中的数据合并到原数组
-        while (i < n1 && j < n2) {
-            if (leftArray[i] <= rightArray[j]) {
-                arr[k] = leftArray[i];
-                i++;
-            } else {
-                arr[k] = rightArray[j];
+        int i = 0; //left array
+        int j = 0; //right array
+        for(; i < arr.length; i++) {
+            if(i < mid) {
+                leftArray[i] = arr[i];
+            }
+            else {
+                rightArray[j] = arr[i];
                 j++;
             }
-            k++;
         }
 
-        // 如果左子数组还有剩余元素，将它们加到原数组中
-        while (i < n1) {
-            arr[k] = leftArray[i];
+        mergeSort(leftArray);
+        mergeSort(rightArray);
+
+        merge(leftArray,rightArray,arr);
+    }
+
+    // 合并两个子数组的函数
+    public static void merge(int[] leftArray,int[] rightArray,int[] arr) {
+        int i = 0;
+        int l = 0;
+        int r = 0;
+        while (l < leftArray.length && r < rightArray.length) {
+            if (leftArray[l] > rightArray[r]) {
+                arr[i] = rightArray[r];
+                i++;
+                r++;
+            } else {
+                arr[i] = leftArray[l];
+                i++;
+                l++;
+            }
+        }
+
+        while (l < leftArray.length){
+            arr[i] = leftArray[l];
             i++;
-            k++;
+            l++;
         }
 
-        // 如果右子数组还有剩余元素，将它们加到原数组中
-        while (j < n2) {
-            arr[k] = rightArray[j];
-            j++;
-            k++;
+        while (r < rightArray.length){
+            arr[i] = rightArray[r];
+            i++;
+            r++;
         }
     }
 
@@ -83,7 +76,7 @@ public class MergeSort {
         }
 
         //调用归并排序
-        mergeSort(arr, 0, arr.length - 1);
+        mergeSort(arr);
 
         //排序后的数组
         System.out.println("排序后的数组：");
